fe8a93030fc2e205171760db208ff9332e1c6e65
Rayyan Hafeez <dev.rayyanhafeez@gmail.com>
2022-11-19 17:53:53 +0500

    Order List Updated

--- frontend/src/component/Admin/OrderList.js
+++ frontend/src/component/Admin/OrderList.js
@@ -6,7 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAlert } from "react-alert";
 import { Button } from "@material-ui/core";
 import MetaData from "../layout/MetaData";
-import { Delete, Edit } from "@mui/icons-material";
+import { CountertopsTwoTone, Delete, Edit } from "@mui/icons-material";
 import SideBar from "./Sidebar";
 import {
   deleteOrder,
@@ -54,91 +54,22 @@ const OrderList = () => {
     dispatch(getAllUsers());
   }, [dispatch, alert, error, deleteError, navigate, isDeleted]);
 
-  const columns1 = [
-    { field: "itemId", headerName: "User ID", minWidth: 300, flex: 1 },
+  var orderUserIds = [];
+  var OrderuserNames = [];
 
-    {
-      field: "actions",
-      flex: 0.3,
-      headerName: "Actions",
-      minWidth: 150,
-      type: "number",
-      sortable: false,
-      renderCell: (params) => {
-        return (
-          <Fragment>
-            <Link to={`/admin/order/${params.getValue(params.id, "id")}`}>
-              <Edit />
-            </Link>
-
-            <Button
-              onClick={() =>
-                deleteOrderHandler(params.getValue(params.id, "id"))
-              }
-            >
-              <Delete />
-            </Button>
-          </Fragment>
-        );
-      },
-    },
-  ];
-
-  const columns = [
-    { field: "userName", headerName: "Order ID", minWidth: 300, flex: 1 },
-
-    {
-      field: "actions",
-      flex: 0.3,
-      headerName: "Actions",
-      minWidth: 150,
-      type: "number",
-      sortable: false,
-      renderCell: (params) => {
-        return (
-          <Fragment>
-            <Link to={`/admin/order/${params.getValue(params.id, "id")}`}>
-              <Edit />
-            </Link>
-
-            <Button
-              onClick={() =>
-                deleteOrderHandler(params.getValue(params.id, "id"))
-              }
-            >
-              <Delete />
-            </Button>
-          </Fragment>
-        );
-      },
-    },
-  ];
-
-  const rows1 = [];
-  const rows2 = [];
-  const rows = [];
-
-  const userIds = users && users.map((user) => user._id);
-
-  users &&
-    users.forEach((item) => {
-      rows.push({
-        id: item._id,
-        name: item.name,
-        email: item.email,
-      });
-    });
-
+  var Ordercounter = 0;
   orders &&
     orders.forEach((order) => {
-      console.log("order id:", order._id);
+      orderUserIds[Ordercounter] = order._id;
       users &&
         users.forEach((userData) => {
           if (order.user === userData._id) {
-            console.log("ordered by", userData.name);
+            OrderuserNames[Ordercounter] = userData.name;
           }
         });
+      Ordercounter++;
     });
+  var OrderCounter1 = 0;
   return (
     <Fragment>
       <MetaData title={`ALL ORDERS - Admin`} />
@@ -147,22 +78,12 @@ const OrderList = () => {
         <SideBar />
         <div className="productListContainer">
           <h1 id="productListHeading">ALL ORDERS</h1>
-          <DataGrid
-            rows={rows1}
-            columns={columns}
-            pageSize={10}
-            disableSelectionOnClick
-            className="productListTable"
-            autoHeight
-          />
-          <DataGrid
-            rows={rows2}
-            columns={columns1}
-            pageSize={10}
-            disableSelectionOnClick
-            className="productListTable"
-            autoHeight
-          />
+          {orderUserIds.map((order) => (
+            <li key={OrderCounter1++}>
+              Order is {order} & It is Ordered by{" "}
+              {OrderuserNames[OrderCounter1]}
+            </li>
+          ))}
         </div>
       </div>
     </Fragment>
