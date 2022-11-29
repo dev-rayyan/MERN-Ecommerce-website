bc4c5f6d4789dc592eb564d1ae334c0b882d13aa
Rayyan Hafeez <dev.rayyanhafeez@gmail.com>
2022-11-29 20:06:52 +0500

    Sidebar Updated

--- frontend/src/component/Admin/Sidebar.js
+++ frontend/src/component/Admin/Sidebar.js
@@ -20,165 +20,243 @@ import {
   CategoryRounded,
 } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import $ from "jquery";
+import { useEffect } from "react";
 
-const StyledTreeItemRoot = styled(TreeItem)(({ theme }) => ({
-  color: "#edf2f9",
-  [`& .${treeItemClasses.content}`]: {
-    color: "#edf2f9",
-    paddingRight: theme.spacing(1),
-    fontWeight: theme.typography.fontWeightMedium,
-    "&.Mui-expanded": {
-      fontWeight: theme.typography.fontWeightRegular,
-    },
-    "&:hover": {
-      backgroundColor: theme.palette.action.hover,
-    },
-    "&.Mui-focused, &.Mui-selected, &.Mui-selected.Mui-focused": {
-      backgroundColor: `var(--tree-view-bg-color, ${theme.palette.action.selected})`,
-      color: "var(--tree-view-color)",
-    },
-    [`& .${treeItemClasses.label}`]: {
-      fontWeight: "inherit",
-      color: "inherit",
-    },
-  },
-  [`& .${treeItemClasses.group}`]: {
-    marginLeft: 0,
-    [`& .${treeItemClasses.content}`]: {
-      paddingLeft: theme.spacing(2),
-    },
-  },
-}));
-
-function StyledTreeItem(props) {
-  const {
-    bgColor,
-    color,
-    labelIcon: LabelIcon,
-    labelInfo,
-    labelText,
-    ...other
-  } = props;
-
-  return (
-    <StyledTreeItemRoot
-      label={
-        <Box
-          sx={{ display: "flex", alignItems: "center", px: 2, py: 1.5, pr: 0 }}
-        >
-          <Box component={LabelIcon} color="inherit" sx={{ mr: 1 }} />
-
-          {labelText}
-        </Box>
+const Sidebar = () => {
+  useEffect(() => {
+    $(".sidebar-dropdown > a").on("click", function () {
+      $(".sidebar-submenu").slideUp(200);
+      if ($(this).parent().hasClass("active")) {
+        $(".sidebar-dropdown").removeClass("active");
+        $(this).parent().removeClass("active");
+      } else {
+        $(".sidebar-dropdown").removeClass("active");
+        $(this).next(".sidebar-submenu").slideDown(200);
+        $(this).parent().addClass("active");
       }
-      style={{
-        "--tree-view-color": color,
-        "--tree-view-bg-color": bgColor,
-      }}
-      {...other}
-    />
-  );
-}
+    });
 
-StyledTreeItem.propTypes = {
-  bgColor: PropTypes.string,
-  color: PropTypes.string,
-  labelIcon: PropTypes.elementType.isRequired,
-  labelInfo: PropTypes.string,
-  labelText: PropTypes.string.isRequired,
-};
+    $("#close-sidebar").on("click", function () {
+      $(".page-wrapper").removeClass("toggled");
+    });
 
-export default function Sidebar() {
+    $("#show-sidebar").on("click", function () {
+      $(".page-wrapper").addClass("toggled");
+    });
+  }, []);
+
   return (
-    <div className="sidebar">
-      <TreeView
-        aria-label="gmail"
-        defaultExpanded={["2"]}
-        defaultCollapseIcon={<ArrowDropDownIcon />}
-        defaultExpandIcon={<ArrowRightIcon />}
-        defaultEndIcon={<div style={{ width: 24 }} />}
-        sx={{ height: 264, flexGrow: 1, maxWidth: 400, overflowY: "auto" }}
-      >
-        <Link to="/admin/dashboard">
-          <StyledTreeItem
-            color="#182330"
-            bgColor="#edf2f9"
-            nodeId="1"
-            labelText="Dashboard"
-            labelIcon={Dashboard}
-          />
-        </Link>
-        <Link to="/admin/orders">
-          <StyledTreeItem
-            color="#182330"
-            bgColor="#edf2f9"
-            nodeId="2"
-            labelText="Orders"
-            labelIcon={ListAlt}
-          />
-        </Link>
-        <StyledTreeItem
-          color="#182330"
-          bgColor="#edf2f9"
-          nodeId="3"
-          labelText="Products"
-          labelIcon={ImportExport}
-        >
-          <Link to="/admin/products">
-            <StyledTreeItem
-              nodeId="7"
-              labelText="All Products"
-              labelIcon={PostAdd}
-              color="#182330"
-              bgColor="#edf2f9"
-            />
-          </Link>
-          <Link to="/admin/product">
-            <StyledTreeItem
-              nodeId="8"
-              labelText="Create New Product"
-              labelIcon={Add}
-              color="#182330"
-              bgColor="#edf2f9"
-            />
-          </Link>
-        </StyledTreeItem>
-        <StyledTreeItem
-          color="#182330"
-          bgColor="#edf2f9"
-          nodeId="3"
-          labelText="Categories"
-          labelIcon={CategoryRounded}
-        >
-          <Link to="/admin/products">
-            <StyledTreeItem
-              nodeId="7"
-              labelText="All Categories"
-              labelIcon={PostAdd}
-              color="#182330"
-              bgColor="#edf2f9"
-            />
-          </Link>
-          <Link to="/admin/product">
-            <StyledTreeItem
-              nodeId="8"
-              labelText="Create New Category"
-              labelIcon={Add}
-              color="#182330"
-              bgColor="#edf2f9"
-            />
-          </Link>
-        </StyledTreeItem>
-        <Link to="/admin/users">
-          <StyledTreeItem
-            color="#182330"
-            bgColor="#edf2f9"
-            nodeId="4"
-            labelText="Users"
-            labelIcon={People}
-          />
-        </Link>
-      </TreeView>
+    <div class="page-wrapper chiller-theme toggled">
+      <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
+        <i class="fas fa-bars"></i>
+      </a>
+
+      <nav id="sidebar" class="sidebar-wrapper">
+        <div class="sidebar-content">
+          <div class="sidebar-brand">
+            <a href="#">pro sidebar</a>
+            <div id="close-sidebar">
+              <i class="fas fa-times"></i>
+            </div>
+          </div>
+          <div class="sidebar-header">
+            <div class="user-pic" style={{ color: "#fff" }}>
+              <i class="fa fa-user-circle fa-4x" aria-hidden="true"></i>
+            </div>
+            <div class="user-info">
+              <span class="user-name">
+                {" "}
+                <strong>Joe Chien</strong>
+              </span>
+              <span class="user-role">Administrator</span>
+              <span class="user-status">
+                <i class="fa fa-circle"></i> <span>Online</span>
+              </span>
+            </div>
+          </div>
+          <div class="sidebar-search">
+            <div>
+              <div class="input-group">
+                <input
+                  type="text"
+                  class="form-control search-menu"
+                  placeholder="Search..."
+                />
+                <div class="input-group-append">
+                  <span class="input-group-text">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="sidebar-menu">
+            <ul>
+              <li class="header-menu">
+                <span>General</span>
+              </li>
+              <li class="sidebar-dropdown">
+                <a href="#">
+                  <i class="fa fa-tachometer-alt"></i>
+                  <span>Dashboard</span>
+                  <span class="badge rounded-pill bg-warning">New</span>
+                </a>
+                <div class="sidebar-submenu">
+                  <ul>
+                    <li>
+                      <a href="#">
+                        Dashboard 1{" "}
+                        <span class="badge rounded-pill bg-success">Pro</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">Dashboard 2</a>
+                    </li>
+                    <li>
+                      <a href="#">Dashboard 3</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="sidebar-dropdown">
+                <a href="#">
+                  <i class="fa fa-shopping-cart"></i>
+                  <span>E-commerce</span>
+                  <span class="badge rounded-pill bg-danger">3</span>
+                </a>
+                <div class="sidebar-submenu">
+                  <ul>
+                    <li>
+                      <a href="#">
+                        Products <span>Pro</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">Orders</a>
+                    </li>
+                    <li>
+                      <a href="#">Credit cart</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="sidebar-dropdown">
+                <a href="#">
+                  <i class="far fa-gem"></i>
+                  <span>Components</span>
+                </a>
+                <div class="sidebar-submenu">
+                  <ul>
+                    <li>
+                      <a href="#">General</a>
+                    </li>
+                    <li>
+                      <a href="#">Panels</a>
+                    </li>
+                    <li>
+                      <a href="#">Tables</a>
+                    </li>
+                    <li>
+                      <a href="#">Icons</a>
+                    </li>
+                    <li>
+                      <a href="#">Forms</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="sidebar-dropdown">
+                <a href="#">
+                  <i class="fa fa-chart-line"></i>
+                  <span>Charts</span>
+                </a>
+                <div class="sidebar-submenu">
+                  <ul>
+                    <li>
+                      <a href="#">Pie chart</a>
+                    </li>
+                    <li>
+                      <a href="#">Line chart</a>
+                    </li>
+                    <li>
+                      <a href="#">Bar chart</a>
+                    </li>
+                    <li>
+                      <a href="#">Histogram</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="sidebar-dropdown">
+                <a href="#">
+                  <i class="fa fa-globe"></i>
+                  <span>Maps</span>
+                </a>
+                <div class="sidebar-submenu">
+                  <ul>
+                    <li>
+                      <a href="#">Google maps</a>
+                    </li>
+                    <li>
+                      <a href="#">Open street map</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+
+              <li class="header-menu">
+                <span>Extra</span>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fa fa-book"></i>
+                  <span>Documentation</span>
+                  <span class="badge rounded-pill bg-primary">Beta</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fa fa-calendar"></i>
+                  <span>Calendar</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="fa fa-folder"></i>
+                  <span>Examples</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="sidebar-footer">
+          <a href="#">
+            <i class="fa fa-bell"></i>
+            <span class="badge rounded-pill bg-warning notification">3</span>
+          </a>
+          <a href="#">
+            <i class="fa fa-envelope"></i>
+            <span class="badge rounded-pill bg-success notification">7</span>
+          </a>
+          <a href="#">
+            <i class="fa fa-cog"></i>
+            <span class="badge-sonar"></span>
+          </a>
+          <a href="#">
+            <i class="fa fa-power-off"></i>
+          </a>
+        </div>
+      </nav>
+
+      <main class="page-content">
+        <div class="container-fluid">
+          <h2>Pro Sidebar</h2>
+          <hr />
+        </div>
+      </main>
     </div>
   );
-}
+};
+
+export default Sidebar;
